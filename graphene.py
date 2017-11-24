d2912bb512170d5a549e536f9ad907f164c7af68
Viraj Bangari <Viraj3f@gmail.com>
2017-11-23 22:15:55 -0500

    Add saving of plots

--- graphene.py
+++ graphene.py
@@ -1,14 +1,123 @@
 import cv2
+from bresenham import bresenham
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
-def analyze(filename):
+
+outputDirName = "Output"
+
+
+def analyze(filename, p1, p2, imageName=None, show=True):
+    """
+    Pass in a filename, and two tuples containing xy
+    coordinates
+    """
+    if imageName is None:
+        imageName = filename.split("/")[-1].split(".")[0]
+
+    imageDirName = "./" + outputDirName + "/" + imageName + "/"
+
+    if not os.path.exists(imageDirName):
+        os.mkdir(imageDirName)
+
     image = cv2.imread(filename)
-    return
+    plotData(image, p1, p2, imageDirName)
+    # showChannels(image, p1, p2, imageDirName)
+
+
+def plotData(image, p1, p2, imageDirName, show=True):
+    """
+    Plots the RGB channel outputs for the image.
+    """
+    # Get the points on the line
+    points = bresenham(p1[0], p1[1], p2[0], p2[1])
+    redPoints = []
+    greenPoints = []
+    bluePoints = []
+    for x, y in points:
+        red, green, blue = image[y][x]
+        redPoints.append(red)
+        bluePoints.append(blue)
+        greenPoints.append(green)
+
+    redPoints = np.array(redPoints)
+    bluePoints = np.array(bluePoints)
+    greenPoints = np.array(greenPoints)
+    pixelDistances = np.arange(0, len(redPoints))
+
+    # Save in to matriax. Serialize into CSV for later analysis
+    combined = np.zeros((len(redPoints), 4))
+    combined[:, 0] = pixelDistances
+    combined[:, 1] = redPoints
+    combined[:, 2] = greenPoints
+    combined[:, 3] = bluePoints
+
+    np.savetxt(imageDirName + 'RGB_Channels.csv',
+               combined.astype(int),
+               delimiter=",")
+
+    # fig = plt.figure()
+    # ax1 = fig.add_subplot(221)
+    # ax2 = fig.add_subplot(222)
+    # ax3 = fig.add_subplot(223)
+
+    # ax1.plot(pixelDistances, redPoints, 'r')
+    # ax1.set_xlabel("Distance [pixels]")
+    # ax1.set_ylabel("Red Channel Intensity")
+
+    # ax2.plot(pixelDistances, greenPoints, 'g')
+    # ax2.set_xlabel("Distance [pixels]")
+    # ax2.set_ylabel("Green Channel Intensity")
+
+    # ax3.plot(pixelDistances, bluePoints, 'b')
+    # ax3.set_xlabel("Distance [pixels]")
+    # ax3.set_ylabel("Blue Channel Intensity")
+
+    # if show:
+    #     fig.show()
+
+    # fig.savefig(imageDirName + "plots.png")
+
+
+def showChannels(image, p1, p2, imageDirName, show=True):
+    """
+    Displays the line where the image was analyzed
+    """
+    thickness = 5
+    blueChannel, greenChannel, redChannel = cv2.split(image)
+
+    cv2.line(blueChannel, p1, p2, 255, thickness)
+    cv2.line(greenChannel, p1, p2, 255, thickness)
+    cv2.line(redChannel, p1, p2, 255, thickness)
+
+    cv2.imwrite(imageDirName + "blueChannel.png", blueChannel)
+    cv2.imwrite(imageDirName + "redChannel.png", redChannel)
+    cv2.imwrite(imageDirName + "greenChannel.png", greenChannel)
+
+    if show:
+        cv2.namedWindow('Blue', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Blue', 600, 600)
+        cv2.imshow('Blue', blueChannel)
+
+        cv2.namedWindow('Green', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Green', 600, 600)
+        cv2.imshow('Green', greenChannel)
+
+        cv2.namedWindow('Red', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Red', 600, 600)
+        cv2.imshow('Red', redChannel)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 def main():
-    filenames = ["./Photos/DSL30002.TIF"]
-    for filename in filenames:
-        analyze(filename)
+    if not os.path.exists(outputDirName):
+        os.mkdir(outputDirName)
+    # analyze("./Photos/DSL30047.TIF", (1282, 844), (1224, 998))
+    analyze("./Photos/DSL30048.TIF", (1079, 900), (1454, 1092))
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
